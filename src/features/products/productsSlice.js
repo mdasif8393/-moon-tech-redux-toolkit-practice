39fa6df2fbf6278b63cfb4af5616d8b436d71da1
mdasif8393 <md.asif.8393@gmail.com>
2023-02-19 15:14:17 +0600

    add create async thunk

---
+++ src/features/products/productsSlice.js
@@ -0,0 +1,17 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    products: [],
+}
+
+const getProducts = createAsyncThunk("products/getProduct", async () => {
+
+});
+
+const productsSlice = createSlice({
+    name: "products",
+    initialState,
+    extraReducers: {},
+})
+
+export default productsSlice.reducer;
--- src/pages/Main/Home.js
+++ src/pages/Main/Home.js
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from "react";
 import ProductCard from "../../components/ProductCard";
+import { useDispatch, useSelector } from "react-redux";
+import { toggle, toggleBrands } from "../../features/filter/filterSlice";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
+  const filter = useSelector((state)=> state.filter);
+  const {brands, stock} = filter;
+  
 
   useEffect(() => {
     fetch("http://localhost:5000/products")
@@ -10,27 +15,61 @@ const Home = () => {
       .then((data) => setProducts(data.data));
   }, []);
 
+  const dispatch = useDispatch();
+
   const activeClass = "text-white  bg-indigo-500 border-white";
+
+  let content;
+
+  if(products.length){
+    content = products.map((product) => (
+      <ProductCard key={product.model} product={product} />
+    ))
+  }
+
+  if(products.length && (stock || brands.length)){ //if product length, stock = true brand length true
+    content = products
+    .filter((product) => {
+      if(stock){
+        return product.status === true //if stock true then return product status true
+      }
+      return product;
+    })
+    .filter(product => {
+      if(brands.length){
+        return brands.includes(product.brand);
+      }
+      return product;
+    })
+    .map((product) => (
+      <ProductCard key={product.model} product={product} />
+    ))
+  }
 
   return (
     <div className='max-w-7xl gap-14 mx-auto my-10'>
       <div className='mb-10 flex justify-end gap-5'>
         <button
-          className={`border px-3 py-2 rounded-full font-semibold ${activeClass} `}
+        onClick={()=>dispatch(toggle())}
+        className={`border px-3 py-2 rounded-full font-semibold ${stock ? activeClass : null} `}
         >
           In Stock
         </button>
-        <button className={`border px-3 py-2 rounded-full font-semibold`}>
+        <button
+        onClick={()=>dispatch(toggleBrands("amd"))}
+        className={`border px-3 py-2 rounded-full font-semibold  ${brands.includes('amd') ? activeClass : null}`}>
           AMD
         </button>
-        <button className={`border px-3 py-2 rounded-full font-semibold`}>
+        <button
+        onClick={()=>dispatch(toggleBrands("intel"))}
+        className={`border px-3 py-2 rounded-full font-semibold ${brands.includes("intel") ? activeClass : null}`}>
           Intel
         </button>
       </div>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-14'>
-        {products.map((product) => (
-          <ProductCard key={product.model} product={product} />
-        ))}
+        {
+          content
+        }
       </div>
     </div>
   );
